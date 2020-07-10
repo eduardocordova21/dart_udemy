cd70d5600c90939f4ded424f5e972610fc9f9eb6
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2020-07-10 01:56:08 -0300

    Variáveis

--- bin/dart_udemy.dart
+++ bin/dart_udemy.dart
@@ -7,4 +7,28 @@ void main(List<String> arguments) {
   /** Comentário em Bloco */
 
   // Comentário em Linha
+
+  //Váriavel genérica
+  var nome = 'Eduardo Córdova';
+
+  //Variáveis númericas
+  int idade = 27;
+  double peso = 89.3;
+  num numeroDaSorte = 21;
+
+  //Conversão de tipos
+  int texto1 = int.parse('12');
+  double texto2 = double.parse('21');
+
+  //Captura de erro
+  int erro1 = int.parse('28.6', onError: (source) {
+    return 0;
+  });
+
+  //Captura de erro com Arrow Function
+  int erro2 = int.parse('28.6', onError: (source) => 0);
+
+  //Calculando algo
+  int anosDeCachorro = 7;
+  int idadeDoCachorro = anosDeCachorro * idade;
 }
