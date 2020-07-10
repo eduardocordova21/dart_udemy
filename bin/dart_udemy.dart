998257fe9b7f94b29b9fbdb3b949f3d44d8e9724
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2020-07-10 01:29:11 -0300

    first commit

---
+++ bin/dart_udemy.dart
@@ -0,0 +1,5 @@
+import 'package:dart_udemy/dart_udemy.dart' as dart_udemy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_udemy.calculate()}!');
+}
---
+++ test/dart_udemy_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_udemy/dart_udemy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
