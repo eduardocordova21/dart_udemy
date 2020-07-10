b616c3a319dabc0258346fac41d02b12ef2fcfa8
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2020-07-10 01:40:12 -0300

    Exemplos de comentários.

--- bin/dart_udemy.dart
+++ bin/dart_udemy.dart
@@ -1,5 +1,10 @@
 import 'package:dart_udemy/dart_udemy.dart' as dart_udemy;
 
 void main(List<String> arguments) {
+  // Mensagem impressa no console que executa o método exportado
   print('Hello world: ${dart_udemy.calculate()}!');
+
+  /** Comentário em Bloco */
+
+  // Comentário em Linha
 }
